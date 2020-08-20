94e6e0de871333b18fd95c5971465f5e6121cef1
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-08-20 15:47:09 -0400

    Mapping Returned Data

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -18,6 +18,11 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
   .pipe(
+    map(products => products.map(product => ({
+      ...product,
+      price: product.price * 1.5,
+      SearchKey: [product.productName]
+    }) as Product)),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
