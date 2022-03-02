2d280313011e02bc2c9d36e7c344f59880f71d3d
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-03-02 12:18:33 -0600

    ...

--- APM-Start/src/app/product-categories/product-category.service.ts
+++ APM-Start/src/app/product-categories/product-category.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, tap, ConnectableObservable, catchError } from 'rxjs';
+import { ProductCategory } from "./product-category";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCategoryService {
   private productCategoriesUrl = 'api/productCategories';
+  productCategory$ = this.http.get<ProductCategory[]>(this.productCategoriesUrl)
+  .pipe(
+    tap(data  => console.log('categories', JSON.stringify(data))),
+    catchError(this.handleError)
+  )
 
   constructor(private http: HttpClient) { }
+
 
   private handleError(err: HttpErrorResponse): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
